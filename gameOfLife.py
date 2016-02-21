51f36f212130ad959758249d4b8a34363f39331b
Ruan Moolman <ruan.moolman@stonethree.com>
2016-02-21 22:12:40 +0200

    Remove __repr__ function

--- gameOfLife.py
+++ gameOfLife.py
@@ -3,8 +3,6 @@ from itertools import product
 
 class GameOfLife:
     _cells = set()
-    _view_width = 5
-    _view_height = 5
 
     @property
     def width(self):
@@ -17,13 +15,6 @@ class GameOfLife:
     def __init__(self):
         self._cells = set()
         pass
-
-    def __repr__(self):
-        world_string = ['.' * self._view_width] * self._view_height
-        for x, y in self.cell_iter():
-            if self.is_alive(x, y):
-                world_string[x - 1] = world_string[x - 1][:y - 1] + '*' + world_string[x - 1][y:]
-        return '\n'.join(world_string) + '\n\n'
 
     def set_alive(self, x_coord, y_coord):
         self._cells.add((x_coord, y_coord))
@@ -61,7 +52,3 @@ class GameOfLife:
             return live_neighbours == 3
 
         return False
-
-    def cell_iter(self):
-        for x, y in product(range(self._view_width), range(self._view_height)):
-            yield x + 1, y + 1
